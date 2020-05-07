0abe3880b87eef687ef7f2fdf08836c63d56de75
Josefin Fundin <josefin@fundin.com>
2020-05-07 19:14:31 +0100

    refactoring, adding error alert, use previous fetched data if API call is unsuccessful

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'views/weather_view.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(WeatherApp());
 
 
 
--- lib/views/weather_view.dart
+++ lib/views/weather_view.dart
@@ -1,30 +1,236 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 import 'package:weatherapp/viewmodel/weather_data.dart';
 import 'package:weatherapp/presenter/weather_presenter.dart';
 
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
+class WeatherApp extends StatefulWidget {
+  WeatherApp({Key key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
+  _WeatherAppState createState() => _WeatherAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _WeatherAppState extends State<WeatherApp> {
   UpdateWeather updateWeather = new UpdateWeather();
   Future<AllWeatherData> allWeatherData;
+  AllWeatherData latestWeatherData;
 
   @override
   void initState() {
     super.initState();
-    allWeatherData = updateWeather.getWeatherData();
+    _fetchNewData();
   }
 
   void _fetchNewData() {
     setState(() {
       allWeatherData = updateWeather.getWeatherData();
-      print("pressed a button");
     });
+    allWeatherData.then((weatherData) => latestWeatherData = weatherData)
+        .catchError((a)  {
+      Fluttertoast.showToast(
+          msg: "Unable to fetch new data",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    });
+  }
+
+  Widget currentWeather(context, AllWeatherData data) {
+    return Expanded(
+      child: Container(
+        color: Theme
+            .of(context)
+            .backgroundColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  data.currentWeather.location.toUpperCase(),
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                Text(data.currentWeather.dateTime.toUpperCase()),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(
+                  data.currentWeather.icon,
+                  size: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.05,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(data.currentWeather.description.toUpperCase()),
+                ),
+              ],
+            ),
+            Text(
+              '${data.currentWeather.temperature.round()}°C',
+              style: TextStyle(
+                  fontSize: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.09),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text('HIGH'),
+                      Text(
+                          '${data.fiveDayForecast.todayForecast.tempMax
+                              .round()}°C')
+                    ],
+                  ),
+                ),
+                Container(
+                    height: 20,
+                    child:
+                    VerticalDivider(color: Theme
+                        .of(context)
+                        .primaryColor)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text('LOW'),
+                      Text(
+                          '${data.fiveDayForecast.todayForecast.tempMin
+                              .round()}°C')
+                    ],
+                  ),
+                ),
+                Container(
+                    height: 20,
+                    child:
+                    VerticalDivider(color: Theme
+                        .of(context)
+                        .primaryColor)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text('HUMIDITY'),
+                      Text('${data.currentWeather.humidity}%')
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget weatherForecast(context, AllWeatherData data) {
+    return Container(
+      color: Theme
+          .of(context)
+          .accentColor,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 9.0, right: 8.0),
+        child: Container(
+          color: Theme
+              .of(context)
+              .accentColor,
+          height: 250,
+          child: ListView.separated(
+              separatorBuilder: (BuildContext context, int index) =>
+                  Divider(height: 7, color: Theme
+                      .of(context)
+                      .primaryColor),
+              itemCount: data.fiveDayForecast.forecast.length,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  color: Theme
+                      .of(context)
+                      .accentColor,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            width: 50.0,
+                            child: Text(data
+                                .fiveDayForecast.forecast[index].weekday
+                                .toUpperCase()),
+                          ),
+                          Text(data.fiveDayForecast.forecast[index].description
+                              .toUpperCase()),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            data.fiveDayForecast.forecast[index].icon,
+                            size: 30.0,
+                          ),
+                          Padding(
+                            padding:
+                            const EdgeInsets.only(right: 10.0, left: 10.0),
+                            child: Container(
+                                width: 40.0,
+                                alignment: Alignment.centerRight,
+                                child: Text(
+                                    '${data.fiveDayForecast.forecast[index]
+                                        .tempMax.round()}°C')),
+                          ),
+                          Container(
+                              height: 20,
+                              width: 10.0,
+                              alignment: Alignment.centerRight,
+                              child: VerticalDivider(
+                                  color: Theme
+                                      .of(context)
+                                      .primaryColor)),
+                          Container(
+                              width: 40.0,
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                  '${data.fiveDayForecast.forecast[index]
+                                      .tempMin.round()}°C')),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }),
+        ),
+      ),
+    );
+  }
+
+  Widget skyline(context) {
+    return Container(
+      color: Theme
+          .of(context)
+          .backgroundColor,
+      alignment: Alignment.bottomCenter,
+      height: 132,
+      child: Image(
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        image: AssetImage('assets/london_transparent.png'),
+      ),
+    );
   }
 
   @override
@@ -33,7 +239,7 @@ class _MyAppState extends State<MyApp> {
       title: 'Weather App',
       theme: ThemeData(
           brightness: Brightness.dark,
-          scaffoldBackgroundColor: Colors.black,
+          scaffoldBackgroundColor: Colors.grey[400],
           primaryColor: Colors.white,
           backgroundColor: Colors.grey[400],
           accentColor: Colors.black,
@@ -42,8 +248,9 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Weather App'),
           actions: <Widget>[
-            IconButton(icon: Icon(Icons.refresh, size: 30.0),
-            onPressed: _fetchNewData,
+            IconButton(
+              icon: Icon(Icons.refresh, size: 30.0),
+              onPressed: _fetchNewData,
             )
           ],
         ),
@@ -51,176 +258,20 @@ class _MyAppState extends State<MyApp> {
           child: FutureBuilder<AllWeatherData>(
             future: allWeatherData,
             builder: (context, AsyncSnapshot<AllWeatherData> snapshot) {
-              if (snapshot.hasData) {
+              AllWeatherData data = snapshot.hasData
+                  ? snapshot.data
+                  : latestWeatherData;
+              if (data != null) {
                 return Column(
                   children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        color: Theme.of(context).backgroundColor,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            Column(
-                              children: <Widget>[
-                                Text(snapshot.data.currentWeather.name
-                                    .toUpperCase(), style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
-                                Text(snapshot.data.currentWeather.dateTime.toUpperCase()),
-                              ],
-                            ),
-                            Column(
-                              children: <Widget>[
-                                Icon(
-                                  snapshot.data.currentWeather.weather[0].icon,
-                                  size: 60.0,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(snapshot
-                                      .data.currentWeather.weather[0].description
-                                      .toUpperCase()),
-                                ),
-                              ],
-                            ),
-                            Text(
-                              '${snapshot.data.currentWeather.main.temp.round()}°C',
-                              style: TextStyle(fontSize: 100.0),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 8.0, right: 8.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text('HIGH'),
-                                      Text(
-                                          '${snapshot.data.fiveDayForecast.todayForecast.tempMax.round()}°C')
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                    height: 20,
-                                    child: VerticalDivider(
-                                        color: Theme.of(context).primaryColor)),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 8.0, right: 8.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text('LOW'),
-                                      Text(
-                                          '${snapshot.data.fiveDayForecast.todayForecast.tempMin.round()}°C')
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                    height: 20,
-                                    child: VerticalDivider(
-                                        color: Theme.of(context).primaryColor)),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 8.0, right: 8.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text('HUMIDITY'),
-                                      Text(
-                                          '${snapshot.data.currentWeather.main.humidity}%')
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      color: Theme.of(context).backgroundColor,
-                      alignment: Alignment.bottomCenter,
-                      height: 132,
-                      child: Image(
-                        width: MediaQuery.of(context).size.width,
-                        image: AssetImage('assets/london_transparent.png'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 9.0, right: 8.0),
-                      child: Container(
-                        color: Theme.of(context).accentColor,
-                        height: 250,
-//                      width: 100,
-                        child: ListView.separated(
-                            separatorBuilder: (BuildContext context, int index) =>
-                                Divider(
-                                    height: 7,
-                                    color: Theme.of(context).primaryColor),
-                            itemCount:
-                                snapshot.data.fiveDayForecast.forecast.length,
-                            shrinkWrap: true,
-                            itemBuilder: (BuildContext context, int index) {
-                              return Card(
-                                color: Theme.of(context).accentColor,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    Row(
-                                      children: <Widget>[
-                                        Container(
-                                          width: 50.0,
-                                          child: Text(snapshot
-                                              .data
-                                              .fiveDayForecast
-                                              .forecast[index]
-                                              .weekday
-                                              .toUpperCase()),
-                                        ),
-                                        Text(snapshot.data.fiveDayForecast
-                                            .forecast[index].description
-                                            .toUpperCase()),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        Icon(
-                                          snapshot.data.fiveDayForecast
-                                              .forecast[index].icon,
-                                          size: 30.0,
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.only(right: 10.0, left: 10.0),
-                                          child: Container(
-                                              width: 40.0,
-                                              alignment: Alignment.centerRight,
-                                              child: Text(
-                                                  '${snapshot.data.fiveDayForecast.forecast[index].tempMax.round()}°C')),
-                                        ),
-                                        Container(
-                                            height: 20,
-                                            width: 10.0,
-                                            alignment: Alignment.centerRight,
-                                            child: VerticalDivider(
-                                                color: Theme.of(context).primaryColor)),
-                                        Container(
-                                            width: 40.0,
-                                            alignment: Alignment.centerRight,
-                                            child: Text(
-                                                '${snapshot.data.fiveDayForecast.forecast[index].tempMin.round()}°C')),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }),
-                      ),
-                    ),
+                    currentWeather(context, data),
+                    skyline(context),
+                    weatherForecast(context, data),
                   ],
                 );
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
-
               // By default, show a loading spinner.
               return CircularProgressIndicator();
             },
